c978cc7b46604ddc4c97ee462f4fc27a217ba9cf
Shiro-cha <noum.rzdr@gmail.com>
2022-10-19 16:45:55 +0300

    first commit

---
+++ src/models/schemas/User.js
@@ -0,0 +1,28 @@
+const UserScheme = require("mongoose").Schema({
+  username:{
+    type:String,
+    required:true
+  },
+  name:{
+    type:String,
+    required:true
+  },
+  firstname:{
+    type:String,
+    required:true
+  },
+  email:{
+    type:String,
+    required:true
+  },
+  password:{
+    type:String,
+    required:true
+  },
+  is_verified:{
+    type:String,
+    required:true
+  }
+})
+
+module.exports = require("mongoose").model("user",UserScheme)
