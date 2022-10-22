c23938f98f3b863dcf3c5dcacd728976593daba8
Shiro-cha <noum.rzdr@gmail.com>
2022-10-22 13:06:11 +0300

    Update index.database

--- src/models/index.database.js
+++ src/models/index.database.js
@@ -1,9 +1,10 @@
+require("dotenv").config()
 const mongoose = require("mongoose")
 const db = require("../../config/database.config")
 
 module.exports = function(){
 
-  mongoose.connect(`mongodb://${db.host}:${db.port}/${db.dbname}`)
+  mongoose.connect(process.env.DATABASE)
   mongoose.connection.once("open",function(){
     console.log(db.message)
   }).on("error",function(err){
